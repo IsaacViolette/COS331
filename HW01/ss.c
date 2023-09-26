5492f4e2e622d995a3080aa3c8c2d4d695878cad
IsaacViolette <20isviolette@gmail.com>
2023-09-26 11:51:29 -0400

    Added error checking for length of command line arguments

--- HW01/ss.c
+++ HW01/ss.c
@@ -4,8 +4,14 @@
 
 int main(int argc, char* argv[])
 {
+	if(argc > 5)
+	{
+		printf("Too many command line arguments \n");
+		return 0;
+	}
+
 	char *buf[argc];
-	
+		
 	for(int i = 0; i < argc; i++)
 	{
 		buf[i] = argv[i];
