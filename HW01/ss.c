9c0e90f1878a80132f5a54c62c711ddb515b2118
IsaacViolette <20isviolette@gmail.com>
2023-09-26 20:54:32 -0400

    Program edited and runs fine

--- HW01/ss.c
+++ HW01/ss.c
@@ -4,30 +4,36 @@
 
 int main(int argc, char* argv[])
 {
-	char *buf[argc];
+	char *buf[argc]; //array for child process
 	int ret;
 
+	/* Error checking for command line arguments, assinment asked for a maximum of 5 */
 	if(argc > 5)
 	{
 		printf("Too many command line arguments\n");
 		return -1;
 	}
-		
+	
+	/* Making new array for the child process, removing first argument from argv */	
 	for(int i = 0; i < argc - 1; i++)
 	{
 		buf[i] = argv[i+1];
 	}	
 
+	/*NULL needed in last index for execv command */
 	buf[argc-1] = NULL;
-
+	
+	/* Print all passed command line arguments */
 	printf("I was called with these parameters: ");
 	for(int i = 0; i < argc; i++)
 		printf("%s ", argv[i]);
 	printf("\n");
 
+	/* Create child process */
 	ret = fork();
 
-	if(ret == 0) //child process
+	/* Child process calls execv to run passed program */
+	if(ret == 0)
 	{
 		printf("Child: after fork\n");
 		printf("I am calling execv with parameters :");
@@ -39,8 +45,9 @@ int main(int argc, char* argv[])
 		perror("Execv Error");
 		return -1;
 	}
-
-	else //parent process
+	
+	/* After completion of the child process, parent process continues */
+	else
 	{
 		printf("Waiting on child process with pid = %d to complete\n", ret);
 		wait(NULL);
