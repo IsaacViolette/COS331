10e3b0a05a2a8c8469a14db25ef09ac400c34323
IsaacViolette <20isviolette@gmail.com>
2023-10-04 15:37:46 -0400

    error handling more than one parameter

--- HW02/ss.c
+++ HW02/ss.c
@@ -11,21 +11,32 @@ int main(int argc, char* argv[])
 	while(1)
 	{
 		argi = 0;
+		for(int i = 0; i < 5; i++)
+		{
+			argvec[i] = NULL;
+		}
 
 		printf("$$ ");
 		fflush(stdout);
+
 		ret = read(0, buf, 80);
 		
-		argvec[argi++] = buf;
+		argvec[argi] = buf;
+		argi++;
 
 		for(int i = 0; i < ret; i++)
 		{
-			if(buf[i] == ' ' || buf[i] == '\n')
+			if(buf[i] == ' ')
 			{
 				//maybe add error check if argi is greater than 4
 				buf[i] = '\0';
 				argvec[argi] = &buf[i+1];
 				argi++;
+			}
+			if(buf[i] == '\n')
+			{
+				buf[i] = '\0';
+				break;
 			}
 		}
 		argvec[argi] = NULL;
@@ -35,11 +46,11 @@ int main(int argc, char* argv[])
 		if(ret == 0)//child
 		{
 			execv(argvec[0], argvec);
-			perror("Execv Error");
+			printf("Execv error using file: %s \n", argvec[0]);
 			return -1;
 		}
 
-		else
+		else //parent
 		{
 			wait(NULL);
 		}
