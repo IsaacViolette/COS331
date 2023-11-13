6a7fc66bded79885c061de8a7d5f2d8019fdceda
IsaacViolette <20isviolette@gmail.com>
2023-11-13 16:15:55 -0500

    These are tester files for homework, forgot to add

---
+++ HW02/extra.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(int argc, char* argv[])
+{
+	printf("Hello World!\n");
+	printf("Number of arguments: %d\n", argc);
+	printf("Arguments: %s, %s, %s, %s\n", argv[0], argv[1], argv[2], argv[3]);
+}
