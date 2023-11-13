13e7b1d3d8d51089e625ccb1a689dd50072c4ae3
IsaacViolette <20isviolette@gmail.com>
2023-11-13 16:46:35 -0500

    Fill arrays with random values

--- HW03/one/one.c
+++ HW03/one/one.c
@@ -3,16 +3,30 @@
 
 int main(int argc, char* argv[])
 {
+	if(argc < 2)
+	{
+		printf("Must enter number of threads\n");
+		exit(0);
+	}
+	if(atoi(argv[1]) < 1)
+	{
+		printf("Must enter a number greater than 0\n");
+		exit(0);
+	}
+	
 	int ran_arr[1024][1024];
-	int tot_ran_dig[10];
+	int tot_ran_dig[] = {0,0,0,0,0,0,0,0,0,0};
+	int num_threads = atoi(argv[1]);
 
 	srand(8);
 
-	for(int i = 0; i <= 1024; i++)
+	for(int i = 0; i < 1024; i++)
 	{
-		for(int j = 0; j <= 1024; j++)
+		for(int j = 0; j < 1024; j++)
 		{
 			ran_arr[i][j] = (rand() % 10);
 		}
 	}
+
+
 }
